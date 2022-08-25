795982579d37d947ade9f783c2ef2700cbe7389f
Роман <vertegel2014@yandex.ru>
2022-08-25 16:17:21 +0300

    Добавляет рабочий скрипт для навигации

--- source/js/script.js
+++ source/js/script.js
@@ -7,8 +7,13 @@ toggle.addEventListener('click', function (){
   if (nav.classList.contains('navigation__container--close')) {
     nav.classList.remove('navigation__container--close');
     nav.classList.add('navigation__container--open');
+    toggle.classList.remove('header__toggle--open');
+    toggle.classList.add('header__toggle--close');
+
   } else {
     nav.classList.add('navigation__container--close');
     nav.classList.remove('navigation__container--open');
+    toggle.classList.add('header__toggle--open');
+    toggle.classList.remove('header__toggle--close');
   }
 })
