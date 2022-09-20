08a6f572740d44a81917b33f54b62cf5dff885cd
Роман <vertegel2014@yandex.ru>
2022-09-21 00:49:27 +0300

    Правки по ПП

--- gulpfile.js
+++ gulpfile.js
@@ -130,7 +130,7 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(scripts));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html').on('change', gulp.series(html, reload));
 }
 
 // Build
