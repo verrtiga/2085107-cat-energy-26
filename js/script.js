60c02837e1efad236445dc649287492a4f0c26c0
verrtiga <vertegel0802@icloud.com>
2023-05-31 22:28:33 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let nav = document.querySelector('.navigation__container');
+let toggle = document.querySelector('.header__toggle');
+let head = document.querySelector('.header__toggle');
+
+nav.classList.remove('navigation__container--nojs');
+head.classList.remove('header--nojs');
+
+toggle.addEventListener('click', function (){
+  if (nav.classList.contains('navigation__container--close')) {
+    nav.classList.remove('navigation__container--close');
+    nav.classList.add('navigation__container--open');
+    toggle.classList.remove('header__toggle--open');
+    toggle.classList.add('header__toggle--close');
+
+  } else {
+    nav.classList.add('navigation__container--close');
+    nav.classList.remove('navigation__container--open');
+    toggle.classList.add('header__toggle--open');
+    toggle.classList.remove('header__toggle--close');
+  }
+})
